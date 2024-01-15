f89199b86698422ff1c1d63c84f82afbb3a8a305
Martin Ligabue <martinligabue@gmail.com>
2024-01-15 13:18:05 +0100

    aggiusto le cose che non sai fare bene

--- test.cpp
+++ test.cpp
@@ -1,46 +1,26 @@
-
 #include <iostream>
 #include <vector>
 #include <unistd.h>
 
-
-
-int main(int argc, char *argv[]){
-    std::cout << "Hello, World!" << std::endl;
-    Observer o;
-    Subject s;
-    // Create displays
-
-
-    // Register displays as observers
-    s.addObserver(&o);
-
-    s.Carica();
-
-    return 0;
-}
-
-
+// Dichiarazioni delle classi Observer e Subject
+class Observer;
+class Subject;
 
 class ObserverInterface {
 public:
     virtual void update(int percentuale) = 0;
 };
 
-
 class Observer : public ObserverInterface {
 public:
     void update(int percentuale) {
-        std::cout << "percentuale " << percentuale
-        << std::endl;
+        std::cout << "percentuale " << percentuale << std::endl;
     }
 };
-
 
 class Subject {
 private:
     int percentuale;
-    //vector perchè è dinamico rispetto all'array classico
     std::vector<Observer*> observers;
 
 public:
@@ -54,11 +34,25 @@ public:
         }
     }
 
-
     void Carica() {
-        for (int i = 0; i <100; i++){
+        for (int i = 0; i < 100; i++) {
             notifyAll(i);
             sleep(1);
         }
     }
 };
+
+int main(int argc, char* argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    Observer o;
+    Subject s;
+    
+    // Create displays
+
+    // Register displays as observers
+    s.addObserver(&o);
+
+    s.Carica();
+
+    return 0;
+}
